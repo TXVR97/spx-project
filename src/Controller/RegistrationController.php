c0c3bc9800187515988ef45bd5c5035a38ae8cfa
Tanguy Xavier <tanguy.xavier97@gmail.com>
2022-10-29 22:32:26 +0200

    fix structure

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -40,22 +40,8 @@ class RegistrationController extends AbstractController
 
             $entityManager->persist($user);
             $entityManager->flush();
-
-            // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('spxproject@gmx.fr', '\"Spx Manager\"'))
-                    ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
-            // do anything else you need here, like send an email
-
-            return $userAuthenticator->authenticateUser(
-                $user,
-                $authenticator,
-                $request
-            );
+            return $this->redirectToRoute('app_admin');
+            
         }
 
         return $this->render('registration/register.html.twig', [
